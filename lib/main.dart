2039ca03b85dcd0f26f96f3c0797260a5692b689
farhad <farhad.codes@gmail.com>
2025-09-05 08:52:54 +0600

    fixed background color & edit read me & clean the codes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Notes App",
+      title: "Alarm App",
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeWidget(),
     );
@@ -24,65 +24,54 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Daily Notes',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
-            fontSize: 26,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Card(
-            // elevation: 4,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(6),
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Card(
+              margin: EdgeInsets.all(0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: Image.asset(
+                "assets/sync_rhythm.gif",
+                width: MediaQuery.of(context).size.width * 1,
+                height: MediaQuery.of(context).size.height * 0.5,
+                fit: BoxFit.cover,
+              ),
             ),
-            child: Column(
-              children: [
-                Center(
-                  child: Image.asset(
-                    "assets/sync_rhythm.gif",
-                    width: MediaQuery.of(context).size.width * 1,
-                    height: MediaQuery.of(context).size.height * 0.5,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(height: 26),
-                Text(
-                  'Welcome to Daily Notes!',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 26),
+            SizedBox(height: 26),
+            Text(
+              'Welcome to Daily Notes!',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 26),
 
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 1,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(6),
-                      backgroundColor: Colors.deepPurple,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
-                      ),
-                    ),
-                    child: Text("NEXT", style: TextStyle(color: Colors.white)),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 1,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(6),
+                  backgroundColor: Colors.deepPurple,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0),
                   ),
                 ),
-              ],
+                child: Text("NEXT", style: TextStyle(color: Colors.white)),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
